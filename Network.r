dc898b086122381bb1125a8abcae8a5d50f82a21
Cody Lewis <codymlewis@protonmail.com>
2019-01-01 16:54:30 +1100

    Fixed malicious assignment

--- Network.r
+++ Network.r
@@ -5,14 +5,16 @@
 # An IoT network simulator
 
 main <- function() {
+	total_nodes = 200
+	malicious_percent = 0.1
     net = data.frame(
-		id = seq(1, 200),
-		energy = rep(100, each=200),
-		QR = rep(1, each=200),
-		services = rep(1, each=200),
-		malicious = floor(runif(200, min=0, max=2)),
-		reputation = rep(1, each=200),
-		R_QR = runif(200)
+		id = seq(1, total_nodes),
+		energy = rep(100, each=total_nodes),
+		QR = rep(1, each=total_nodes),
+		services = rep(1, each=total_nodes),
+		malicious = c(rep(FALSE, each=(total_nodes * (1 - malicious_percent))), rep(TRUE, each=(total_nodes * malicious_percent))),
+		reputation = rep(1, each=total_nodes),
+		R_QR = runif(total_nodes)
     )
     print("The network is:")
     print(net)
