73a0695e9d2c10a55ff8538e26c14360d1b594ae
Cody Lewis <codymlewis@protonmail.com>
2019-01-04 12:42:08 +1100

    Gets a most trusted list

--- TrustModel.r
+++ TrustModel.r
@@ -68,22 +68,25 @@ weigh_reports <- function(lambda, theta, node_reports, report_distances, time) {
 
 # Find the trust values of the proxies
 compute_trust <- function(R, w) {
-    T = c()
+    trust_vals = c()
     for(i in seq(1, length(w))) {
         numerator = 0
         denominator = 0
         for(j in seq(1, length(w[[i]]))) {
             if(w[[i]][[j]] != RESTRICTED_REPORT) {
-                numerator = numerator + (as.numeric(w[[i]][[j]])  * R[[i]]$note[j]) #* R[[i]]$quality_of_recommendation[j]
+                numerator = numerator + (as.numeric(w[[i]][[j]]) * R[[i]]$quality_of_recommendation[j] * R[[i]]$note[j])
                 denominator = denominator + as.numeric(w[[i]][[j]])
             }
         }
-        T[i] = ifelse(denominator == 0,
-                -2,
+        trust_vals[i] = ifelse(denominator == 0,
+                0,
                 numerator / denominator
             )
     }
-    T
+    data.frame(
+	id = seq(1, length(w)),
+	trust = trust_vals
+    )
 }
 
 # Select suitable entities for a target service
@@ -97,28 +100,31 @@ entity_selection <- function(lambda, theta, eta, R, s_target, c_target, time) {
                         weigh_reports(lambda, theta, R[[i]], d[[i]], time))
     }
     T = compute_trust(R, w)
-    print(T)
+    T[order(-T$trust),]$id
 }
 
 # Return the note value based on how a proxy will perform on a transaction
 take_note <- function(network, service_target, capability_target, proxy_id) {
     if(network$malicious[proxy_id]) {
-	    -1
+	-1
     } else if(network$service[proxy_id] < service_target ||
 		      network$capability[proxy_id] < capability_target) {
-	    0
+	0
     } else {
-	    1
+	1
     }
 }
 
 # Simulate a transaction, add a report entry based on that
-transaction <- function(network, service_target, capability_target, proxy_id, reports, time) {
+transaction <- function(network, service_target, capability_target, proxy_id, reports, time, is_bootstrapping) {
     j = length(reports$service) + 1
     reports$service[j] = service_target
     reports$capability[j] = capability_target
     reports$note[j] = take_note(network, service_target, capability_target, proxy_id)
     reports$time[j] = time
+    if(is_bootstrapping) {
+	reports$quality_of_recommendation[j] = 1
+    }
     reports
 }
 
@@ -126,9 +132,10 @@ transaction <- function(network, service_target, capability_target, proxy_id, re
 initialize <- function(network, bootstrap_time, R, time) {
 	for(i in seq(1, bootstrap_time)) {
 	    time = time + 1
-	    proxy_id = floor(runif(1, min=1, max=nrow(network) + 1))
+	    proxy_id = floor(runif(1, min=1, max=length(network$service) + 1))
 	    R[[proxy_id]] = transaction(network, floor(runif(1, min=1, max=101)),
-					floor(runif(1, min=1, max=101)), proxy_id, R[[proxy_id]], time)
+					floor(runif(1, min=1, max=101)), proxy_id, R[[proxy_id]],
+					time, TRUE)
 	}
 	R
 }
@@ -149,27 +156,27 @@ main <- function() {
     total_nodes = 200
     malicious_percent = 0.1
     time = 0
-    network = data.frame(
+    network = list(
 	id = seq(1, total_nodes),
 	energy = rep(100, each=total_nodes),
-	QR = rep(1, each=total_nodes),
 	service = floor(runif(200, min=1, max=101)),
 	capability = floor(runif(200, min=1, max=101)),
 	malicious = c(rep(FALSE, each=(total_nodes * (1 - malicious_percent))),
 				  rep(TRUE, each=(total_nodes * malicious_percent))),
-	reputation = rep(1, each=total_nodes),
-	R_QR = runif(total_nodes)
+	reputation = rep(1, each=total_nodes)
     )
     R = list()
     for(i in seq(1, total_nodes)) {
-	R[[i]] = list(c(), c(), c(), c())
-	names(R[[i]]) <- c("service", "capability", "note", "time")
+	R[[i]] = list(c(), c(), c(), c(), c())
+	names(R[[i]]) <- c("service", "capability", "note", "time", "quality_of_recommendation")
     }
     bootstrap_time = total_nodes * 100
-    R = initialize(network, total_nodes * 100, R, time)
+    R = initialize(network, bootstrap_time, R, time)
     time = time + bootstrap_time
+    print("Reports")
     print(R)
-    entity_selection(lambda, theta, eta, R, 50, 50, time)
+    print("Most trusted nodes")
+    print(entity_selection(lambda, theta, eta, R, 50, 50, time))
 }
 
 main()
