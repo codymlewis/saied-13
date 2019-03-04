0c1a0fbb21e3721f12db8fef07b0db371e156a38
Cody Lewis <codymlewis@protonmail.com>
2019-03-04 11:35:48 +1100

    Report now provides data on the 5% increments

--- report/FillAttackFigures.sh
+++ report/FillAttackFigures.sh
@@ -13,9 +13,9 @@ FIGURE_TEMPLATE=$(sed "s/graph_folder/$GRAPH_FOLDER/" AttackFigures.tex)
 FIGURE_TEMPLATE="${FIGURE_TEMPLATE//rep_folder/$REP_FOLDER}"
 FIGURE_TEMPLATE="${FIGURE_TEMPLATE//attack_folder/$ATTACK_FOLDER}"
 
-FIGURES="${FIGURE_TEMPLATE//mal_num/10}"
+FIGURES="${FIGURE_TEMPLATE//mal_num/5}"
 
-for i in {20..90..10}; do
+for i in {10..95..5}; do
     FIGURES="$FIGURES
     ${FIGURE_TEMPLATE//mal_num/$i}"
 done
